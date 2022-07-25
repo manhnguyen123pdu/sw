52c1e2cf4a494e8d98582501674baf322f979dbd
your name <manhnguyen.tla@gmail.com>
2022-07-25 20:54:59 +0700

    s

---
+++ sw.js
@@ -0,0 +1,39 @@
+// let browser = prompt("xin moi nhap");
+// switch (browser) {
+//     case 'Edge':
+//         alert("You've got the Edge!");
+//         break;
+//     case 'Chorme':
+//     case 'Opera':
+//     case 'safari':
+//         alert('Okay we support these browsers too');
+//         break;
+//     default:
+//         alert('We hope that this page looks ok!');
+// }
+
+
+// let a= prompt('xin moi nhap a');
+// switch(a){
+//     case '0':
+//         alert('0');
+//         break;
+//     case '1':
+//         alert ('1');
+//         break;
+//     case '2':
+//     case '3':
+//         alert('2,3');
+//         break;
+// }
+
+let a=prompt('Type a number', 0);
+if(a>0){
+    alert('1')
+}
+else if( a<0){
+    alert('-1')
+}
+else{
+    alert('0')
+}
